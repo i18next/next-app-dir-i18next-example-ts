14c885d6e5b6e7176f792e2387a69792aa570743
Scott Yinan Fan <scottvan2008@gmail.com>
2025-02-03 11:54:05 +0800

    change language

--- app/i18n/settings.ts
+++ app/i18n/settings.ts
@@ -1,5 +1,5 @@
 export const fallbackLng = 'en'
-export const languages = [fallbackLng, 'de', 'zh']
+export const languages = [fallbackLng, 'zh', 'pt']
 export const defaultNS = 'translation'
 export const cookieName = 'i18next'
 
